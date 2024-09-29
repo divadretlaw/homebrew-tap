244960f6c54e13731fb7c184c05d0c969e6d5fdd
David Walter <hello@davidwalter.at>
2024-09-29 22:46:13 +0200

    Update very

--- Formula/very.rb
+++ Formula/very.rb
@@ -2,8 +2,8 @@
 class Very < Formula
   desc 'A collection of useful commandline tools'
   homepage 'https://github.com/divadretlaw/very'
-  url 'https://github.com/divadretlaw/very/releases/download/3.2.0/very.zip'
-  sha256 'daa98baec337b10d71f95458492936cd8f3879463bc3f4cff5933a7e2f298f2e'
+  url 'https://github.com/divadretlaw/very/releases/download/3.2.1/very.zip'
+  sha256 'dfaafdecf9c416e7104d62bd8e6c430523a8465eebb84748f13ae00abbe73edc'
 
   def install
     bin.install 'very'
