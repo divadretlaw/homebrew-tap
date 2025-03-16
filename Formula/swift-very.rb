5189d5cbc5d145184a385f74cfb00c608d4cfc64
David Walter <hello@davidwalter.at>
2025-03-16 15:27:43 +0100

    Update swift-very

--- Formula/swift-very.rb
+++ Formula/swift-very.rb
@@ -1,8 +1,8 @@
 class SwiftVery < Formula
   desc 'A collection of useful swift package tools'
   homepage 'https://github.com/divadretlaw/swift-very'
-  url 'https://github.com/divadretlaw/swift-very/releases/download/0.4.0/swift-very.zip'
-  sha256 '202e37ecb27717deeb694d80962c6c1b0f2f7b438d97c04b7ec3e94e146be936'
+  url 'https://github.com/divadretlaw/swift-very/releases/download/0.5.0/swift-very.zip'
+  sha256 'a59d85dc1a721b57df5ee6e66a0d371d2ab7831d8466591cb2bbfb0095a18fc2'
 
   def install
     bin.install 'swift-very'
